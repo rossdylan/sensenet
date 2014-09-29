f30558deb4f0397707f75714f875061b2826953e
Ross Delinger <rossdylan@csh.rit.edu>
2014-09-29 11:18:35 -0400

    changed how I'm doing things, using zeromq sockets and threads to communicate and keeping track of sensor data in a Map

--- main.hs
+++ main.hs
@@ -1,17 +1,69 @@
-import qualified Data.Set as Set
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+--import System.ZMQ4.Monadic
+import Control.Monad (forever)
+import Control.Concurrent
+import Control.Concurrent.STM
+import Data.ByteString.Char8 (pack, unpack)
+import qualified Data.Map.Strict as Map
+import Data.Aeson
+import Control.Applicative
+import Control.Monad
+-- alias for zmq uris
+type ZmqUri = String
+
+-- sensor data used to transport data over the network
+data SensorData = SensorData {
+    hostName :: String, 
+    sData :: Float }
+
+instance FromJSON SensorData where
+    parseJSON (Object v) = SensorData <$>
+                            v .: "hostname" <*>
+                            v .: "data"
+    parseJSON _ = mzero
+
+instance ToJSON SensorData where
+    toJSON (SensorData hname sdata) = object ["hostname" .= hname, "data" .= sdata]
+
+-- map hostnames to a list of floats
+type SensorMap = Map.Map String [Float]
+
+-- STM Wrapper
+type GlobalMap = TVar SensorMap
+
+-- Helper function to atomically read a TVar
+atomRead = atomically . readTVar
+
+-- Function that adds some sensor data to the global sensor data map
+mapUpdate :: SensorData -> SensorMap -> SensorMap
+mapUpdate sd m = Map.update (\val-> Just (sData sd : val)) (hostName sd) m
 
 
-class LWWSet s where
-    add     :: Eq a => s -> a -> s
-    remove  :: Eq a => s -> a -> s
-    exists  :: Eq a => s -> a -> Bool
-    merge   :: s -> s -> s
-    asList  :: Eq a => s -> [a]
+-- Atomically add some data to the global data map
+addSensorData :: GlobalMap -> SensorData -> IO ()
+addSensorData gd sd = do
+    globular <- atomRead gd
+    atomically $ writeTVar gd $ mapUpdate sd globular
 
-type Hostname = String
-type SensorValue = Float
+-- Atomically get all sensor data for a hostname
+getSensorData :: GlobalMap -> String -> IO (Maybe [Float])
+getSensorData gd hostname = do 
+    globular <- atomRead gd
+    globular >>= Map.lookup hostname
+-- Thread to listen for peeps over the network
 
-data SensorEntry = SensorEntry Hostname SensorValue 
--- data SensorSet = Set.Set SensorEntry
+{-
+remoteListener :: GlobalMap -> ZmqUri -> IO ()
+remoteListener gd uri = runZMQ $ do
+    pub <- socket Pub
+    bind pub uri
+    forever $ do
+        line <- liftIO $ fromString <$> getLine
+        
 
-main = do return ()
+-}
+main = return ()
+
+
